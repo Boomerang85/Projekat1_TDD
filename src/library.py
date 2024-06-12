6cdca20af64a6ab31a63ed75ad9b3ebf6451dcd9
Boomerang85 <whiteluke06@gmail.com>
2024-06-12 12:04:27 +0200

    first commit

---
+++ src/library.py
@@ -0,0 +1,6 @@
+class Library:
+    def __init__(self):
+        self.objectList = []
+    def add(self, book):
+        self.objectList.append(book)
+    
---
+++ src/main.py
@@ -0,0 +1,29 @@
+from book import Book
+from library import Library
+
+library = Library()
+
+insert = input("\nDobrodošli u biblioteku. Šta biste želeli da uradite? \n\n1) Dodaj knjigu \n")
+
+match insert:
+
+    case "1":
+        print("Koliko knjiga biste želeli da dodate?")
+        
+        n = int(input())
+        
+        i = 0
+
+        while i < n:
+            print ("Unesite naziv, autora, godinu izdanja i žanr knjige.\n")
+            title = input("Naziv: ")
+            author = input("Autor: ")
+            releaseDate = input("Godina izdanja: ")
+            genre = input("Žanr: ")
+            book = Book(title, author, releaseDate, genre)
+            library.add(book)
+            print("Vaša knjiga je uspešno dodata u biblioteku.\n")
+            i = i + 1
+
+    case _:
+        print("Molimo Vas odaberite validnu opciju.\n")
